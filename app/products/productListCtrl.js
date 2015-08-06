ac2a712d34aeffd0b4c99cb619210f4fb9e04a3d
anton-b117 <anton@maroonspear.com>
2015-08-06 15:28:14 -0400

    Added product list controller, angular resource, implementing mock backend.

--- app/products/productListCtrl.js
+++ app/products/productListCtrl.js
@@ -3,35 +3,39 @@
   angular
     .module("productManagement")
     .controller("ProductListCtrl",
-                ProductListCtrl);
+                ["productResource",
+                ProductListCtrl]);
 
-  function ProductListCtrl() {
+  function ProductListCtrl(productResource) {
     var vm = this;
-    vm.products = [
-      {
-        "productId": 1,
-        "productName":"Leaf Rake",
-        "productCode":"GDN-0011",
-        "releaseDate":"03/19/2009",
-        "description":"Leaf Rake with 48-inch handle.",
-        "cost":"9.00",
-        "price":"19.95",
-        "catagory":"garden",
-        "tags":["leaf", "tool"],
-        "imageUrl":"http://placehold.it/350x150"
-      },
-      {
-        "productId": 2,
-        "productName":"Hammer",
-        "productCode":"TBX-0048",
-        "releaseDate":"05/21/2013",
-        "description":"Curved claw steel hammer.",
-        "cost":"1.00",
-        "price":"8.95",
-        "catagory":"toolbox",
-        "tags":["tool"],
-        "imageUrl":"http://placehold.it/350x150"
-      }];
+    // vm.products = [
+    //   {
+    //     "productId": 1,
+    //     "productName":"Leaf Rake",
+    //     "productCode":"GDN-0011",
+    //     "releaseDate":"03/19/2009",
+    //     "description":"Leaf Rake with 48-inch handle.",
+    //     "cost":"9.00",
+    //     "price":"19.95",
+    //     "catagory":"garden",
+    //     "tags":["leaf", "tool"],
+    //     "imageUrl":"http://placehold.it/350x150"
+    //   },
+    //   {
+    //     "productId": 2,
+    //     "productName":"Hammer",
+    //     "productCode":"TBX-0048",
+    //     "releaseDate":"05/21/2013",
+    //     "description":"Curved claw steel hammer.",
+    //     "cost":"1.00",
+    //     "price":"8.95",
+    //     "catagory":"toolbox",
+    //     "tags":["tool"],
+    //     "imageUrl":"http://placehold.it/350x150"
+    //   }];
+    productResource.query(function(data) {
+      vm.products = data;
+    });
 
     vm.showImage = false;
 
