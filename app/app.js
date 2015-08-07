9e3c5938e075398977244a7d5a0a1bfb73eea821
anton-b117 <anton@maroonspear.com>
2015-08-07 17:56:56 -0400

    Added productResource.  Bug somewhere in this commit, searching.

--- app/app.js
+++ app/app.js
@@ -2,5 +2,17 @@
   "use strict";
   var app = angular.module("productManagement",
                           ["common.services",
+                           "ui.router",
                            "productResourceMock"]);
+
+   app.config(["$stateProvider",
+                function($stateProvider) {
+                  $stateProvider
+                    .state("productList", {
+                      url: "/products",
+                      templateUrl: "app/products/productListView.html",
+                      controller: "ProductListCtrl as vm"
+                    })
+                }]
+              );
 }());
