6a1a49f4d79889c6dad69b844254047090c3910b
anton-b117 <anton@maroonspear.com>
2015-08-13 07:09:18 -0400

    Added ui-bootstrap date component

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@
 													 ["common.services",
 														"ui.router",
 														"ui.mask",
+														"ui.bootstrap",
 														"productResourceMock"]);
 
   app.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
